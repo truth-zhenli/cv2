815fc547df97a878121c8a44726c6bcb609638c1
Zhen Li <33314620+truth-zhenli@users.noreply.github.com>
2019-10-24 15:41:18 +0800

    0

--- data/data.js
+++ data/data.js
@@ -1,10 +1,6 @@
 
 // Highlight Slides
 var slide_list = [{
-    "source": "images/research/A10.gif",
-    "title": "Membrane remodeling: effect of lipid components",
-    "url": "",
-}, {
     "source": "images/research/A10.jpg",
     "title": "Interaction morphology of nanosheets and lipid membranes",
     "url": "",
@@ -29,6 +25,10 @@ var slide_list = [{
     "title": "The lipid spreading behavior that leads to collapse of liposomes",
     "url": "",
 }, {
+    "source": "images/research/A10.gif",
+    "title": "Membrane remodeling: effect of lipid components",
+    "url": "",
+}, {
     "source": "images/research/A9.jpg",
     "title": "Diffusion of metal ions in polymer electrolytes",
     "url": "",
@@ -39,70 +39,6 @@ var slide_list = [{
 }, {
     "source": "images/research/A16.jpg",
     "title": "Interesting nanostructures obtained with strategies tuning the self-assembly of amphiphiles",
-    "url": "",
-}, {
-    "source": "images/research/A10.gif",
-    "title": "细胞膜重塑及分裂机制：membrane tethering的形成和分裂",
-    "url": "",
-}, {
-    "source": "images/research/A11.gif",
-    "title": "新型纳米结构、器件设计：用于DNA/蛋白测序、气体分离、海水淡化等",
-    "url": "",
-}, {
-    "source": "images/activity/B5.jpg",
-    "title": "2018复杂体系计算统计力学研讨会，中国科学技术大学，合肥，2018.12.7-10",
-    "url": "",
-}, {
-    "source": "images/activity/B6.jpg",
-    "title": "From Computational Biophysics to Systems Biology 研讨会，深圳，2018.5.20-23",
-    "url": "",
-}, {
-    "source": "images/activity/B7.jpg",
-    "title": "第七届国际分子模拟与信息技术应用学术会议，苏州，2014.10.27-28",
-    "url": "",
-}, {
-    "source": "images/activity/B1.jpg",
-    "title": "海报1",
-    "url": "",
-}, {
-    "source": "images/activity/B2.jpg",
-    "title": "海报2",
-    "url": "",
-}, {
-    "source": "images/activity/B3.jpg",
-    "title": "海报3",
-    "url": "",
-}, {
-    "source": "images/activity/B4.jpg",
-    "title": "海报4",
-    "url": "",
-}, {
-    "source": "images/life/H3.jpg",
-    "title": "清晨·香港",
-    "url": "",
-}, {
-    "source": "images/life/H4.jpg",
-    "title": "夜幕将至·香港",
-    "url": "",
-}, {
-    "source": "images/life/H5.jpg",
-    "title": "维港之夜·香港",
-    "url": "",
-}, {
-    "source": "images/life/H6.jpg",
-    "title": "校园小景1·石油大学东营",
-    "url": "",
-}, {
-    "source": "images/life/H7.jpg",
-    "title": "校园小景2·石油大学东营",
-    "url": "",
-}, {
-    "source": "images/life/H1.jpg",
-    "title": "临帖·多宝塔碑",
-    "url": "",
-}, {
-    "source": "images/life/H2.jpg",
-    "title": "篮球",
     "url": "",
 }];
 
